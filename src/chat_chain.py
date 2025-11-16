395bac4bebeba760e6ff3f32c2d154fb6380d413
Gaurav Kumar <hindugauravprataplahotiya@gmail.com>
2025-11-16 11:49:00 +0530

    Update chat_chain.py

--- src/chat_chain.py
+++ src/chat_chain.py
@@ -2,7 +2,8 @@ from langchain_openai import ChatOpenAI
 from langchain_community.vectorstores import FAISS
 from langchain_community.embeddings import HuggingFaceEmbeddings
 
-from langchain.prompts import PromptTemplate
+from langchain_core.prompts import PromptTemplate
+
 from langchain.chains import LLMChain
 from langchain.docstore import InMemoryDocstore
 from langchain.schema import Document
